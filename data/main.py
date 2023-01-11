238afb3278feaf63ec7ca11c23f62eada569f469
aakashrajaraman <20btrcl069@jainuniversity.ac.in>
2023-01-11 12:26:59 +0530

    3

--- data/main.py
+++ data/main.py
@@ -132,7 +132,7 @@ def run():
                 {'name': song3, 'year': year3},
                 {'name': song4, 'year': year4},
                 {'name': song5, 'year': year5}],  data)
-    return render_template('index.html', res = res, len = len(res))
+    return render_template('index.html', res = res)
     
 
 if __name__ == '__main__':
